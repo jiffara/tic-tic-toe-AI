a1551a5dd5f80a05e1d74144917dffe386e85633
Jeffrey Du <jd479@cornell.edu>
2016-08-19 17:26:21 -0400

    started tic tac toe

--- skeleton 2/lib/tic_tac_toe_node.rb
+++ skeleton 2/lib/tic_tac_toe_node.rb
@@ -8,17 +8,36 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    return false if board.winner.nil?
+
   end
 
   def winning_node?(evaluator)
+    return false if board.winner.nil?
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children_array = []
     @board.each_with_index do |row, i|
       row.each_with_index do |pos, j|
         next unless @board[i,j].empty?
+        duped_board = @board.dup
+        duped_board[i,j] = @next_mover_mark
+        children_array << duped_board
+      end
+    end
+    switch_mark
+    children_array
+  end
 
+  def switch_mark
+    @prev_move_pos = @next_mover_mark
+    if @next_mover_mark == :x
+      @next_mover_mark = :o
+    else
+      @next_mover_mark = :x
+    end
   end
 end
